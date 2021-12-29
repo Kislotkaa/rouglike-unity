64c3741bb51f6dff16fd952a6eb1853d09b8f4ca
C2H5OH_BO_MHE <maksihill@yandex.ru>
2021-12-29 16:44:51 +0500

    add

---
+++ Assets/Lib/Scripts/AppModule.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AppModule : MonoBehaviour {
+
+    private void Awake() {
+        InitController();
+        InitDataSource();
+        InitController();
+
+    }
+
+    void InitController(){
+
+    }
+
+    void InitDataSource(){
+
+    }
+}
